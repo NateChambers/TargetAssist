487c9f318aaa2b7a82bc540b54e489645c973c06
turinpt <turinpt@gmail.com>
2016-03-17 01:59:53 +0000

    Key Bindings

--- TargetAssist/TargetAssist.lua
+++ TargetAssist/TargetAssist.lua
@@ -1,4 +1,5 @@
 local lastUpdate = 0
+BINDING_HEADER_TARGETASSIST = "Target Assist";
 
 function TA_OnUpdate()
 	local time = GetTime()
@@ -44,13 +45,24 @@ end
 
 function TA_UpdateIcon(counter,index,target)
 	if target ~= nil then
-		getglobal("TAIcon"..counter):Show()
+		local icon = getglobal("TAIcon"..counter)
+		icon:Show()
 		SetRaidTargetIconTexture(getglobal("TAIcon"..counter.."Texture"),index)
-		getglobal("TAIcon"..counter).target = target
+		icon.target = target
+		icon.index = index
 		return counter + 1
 	else
 		getglobal("TAIcon"..counter):Hide()
 		return counter
+	end
+end
+
+function TA_TargetIcon(index)
+	for i=1,8,1 do
+		local icon = getglobal("TAIcon"..i)
+		if icon:IsVisible() and icon.index ~= nil and icon.index == index then
+			TargetUnit(icon.target)
+		end
 	end
 end
 
